55cb0399e5bac83998a93800ff228161c0c77636
Timoon21 <timoon21sol@gmail.com>
2022-07-19 00:33:16 +0200

    tx example

--- src/cli.ts
+++ src/cli.ts
@@ -3,11 +3,11 @@ import { decodeIx } from "./parser";
 (async () => {
 
   // Some examples for test
-  
+
   //const txSig = '5snAJSJg9Kvt3KpYpixMZQoZfB3FxervpPC3y3jjeShSMQcmcRZDQtQYJzHrFkFY64WmwFhbcGiXmJgx2nQxxRst'  // JUPv3 USDC -> mSOL                     OK
   //const txSig = 'AzHLS6Cy73sQWgkcj1WoYvvSidqY9f78eE9BihCABcQZaVCfHazbH7SZU3F1pKuecei17vPEAMDAZg8T7SiQAVf'   // JUPv1 wrappedSOL -> USDC               OK
   //const txSig = 'GVa9EhEPsuLtBmLDSi7Do5WU99B9cBq7WASVipN3p73rGdN4865KRV9crYXBNNZPLeUDeJ73HxUCMrgZPPkKtci'   // JUPv2 Split Swap  DUST -> wrappedSOL   OK
-  const txSig = 'TqFx6hFRwBG5o1K6D29PKsHeHRbNhUENzZsWHz9sjhMWeqFXoZMQQ3Dhp6QT81yY5dwG8bby57MxDRNRL5NBZQq'  // JUPv3 USDC -> native SOL               OK
+  const txSig = '5Bhj7TujVg255RW2LZCXLCriTExhfpEWjaf8GKt9Q18SnYCXe9rT5YfMANVNik3kbdoYShCEESTrg8yyUAvME1Dc'  // JUPv3 USDC -> native SOL               OK
   //const txSig = '5KdxeiGLjtHHZwZGQFM4t8oAZ8jJn6pA53vPgAHQNca3JQdwVYyEGPvX5XGnDWWuf5XRDZB7QNYT3odqY8TGZb93'  // JUPv2 USDC -> USDC                     NOK - ToDO use innerInstructions (transfert) ?
   //const txSig = '3EHS36VbEvXJEaBZfaHQqd7NnFJTXN5ZxqyNsVLvCagbu16nhSecgirR1VpCz2FgvihbMTgBGPrHFwEfrcZRB9MM'  // JUPv3 USDC -> ETH with PAI dust        OK
 
